e8e32dc9a41049a0fd97de922c1ee9aca8befd9d
psyark <psyark@gmail.com>
2022-09-08 12:19:32 +0900

    再帰呼び出しを廃止

--- parser.go
+++ parser.go
@@ -12,67 +12,68 @@ type taggedText struct {
 	Text string
 }
 
-// パラメータ構造体をパースしてpdftplタグ付きフィールドに対してコールバックを呼び出します
-func parseVars(vars interface{}) ([]taggedText, error) {
-	return parseVarsInternal(reflect.ValueOf(vars), 0, 0)
+type target struct {
+	value   reflect.Value
+	originX float64
+	originY float64
 }
 
-func parseVarsInternal(v reflect.Value, ox float64, oy float64) ([]taggedText, error) {
+// パラメータ構造体をパースしてpdftplタグ付きフィールドに対してコールバックを呼び出します
+func parseVars(vars interface{}) ([]taggedText, error) {
 	texts := []taggedText{}
-	t := v.Type()
 
-	if t.Kind() != reflect.Struct {
-		return nil, fmt.Errorf("vars は構造体である必要があります")
-	}
+	targets := []target{{value: reflect.ValueOf(vars)}}
+	for len(targets) != 0 {
+		tgt := targets[0]
+		targets = targets[1:]
 
-	for i := 0; i < t.NumField(); i++ {
-		f := t.Field(i)
-		tagStr, ok := f.Tag.Lookup("pdftpl")
-		if !ok {
-			continue
+		v := tgt.value
+		t := tgt.value.Type()
+		if t.Kind() != reflect.Struct {
+			return nil, fmt.Errorf("vars は構造体である必要があります")
 		}
 
-		switch f.Type.Kind() {
-		case reflect.String:
-			tag := &textTag{}
-			if err := tag.parse(tagStr); err != nil {
-				return nil, errors.Wrap(err, "parseTag")
+		for i := 0; i < t.NumField(); i++ {
+			f := t.Field(i)
+			tagStr, ok := f.Tag.Lookup("pdftpl")
+			if !ok {
+				continue
 			}
 
-			tt := taggedText{textTag: tag.fromOrigin(ox, oy), Text: v.Field(i).String()}
-			texts = append(texts, tt)
-		case reflect.Array, reflect.Slice:
-			tag := &transTag{}
-			if err := tag.parse(tagStr); err != nil {
-				return nil, errors.Wrap(err, "parseTag")
-			}
-
-			for j := 0; j < v.Field(i).Len(); j++ {
-				fj := float64(j)
-				// 座標を変えつつ再帰呼び出し
-				texts2, err := parseVarsInternal(v.Field(i).Index(j), ox+tag.Dx*fj, oy+tag.Dy*fj)
-				if err != nil {
-					return nil, err
+			switch f.Type.Kind() {
+			case reflect.String:
+				tag := &textTag{}
+				if err := tag.parse(tagStr); err != nil {
+					return nil, errors.Wrap(err, "parseTag")
 				}
-				texts = append(texts, texts2...)
-			}
 
-		case reflect.Struct:
-			tag := &transTag{}
-			if err := tag.parse(tagStr); err != nil {
-				return nil, errors.Wrap(err, "parseTag")
-			}
+				tt := taggedText{textTag: tag.fromOrigin(tgt.originX, tgt.originY), Text: v.Field(i).String()}
+				texts = append(texts, tt)
+			case reflect.Array, reflect.Slice:
+				tag := &transTag{}
+				if err := tag.parse(tagStr); err != nil {
+					return nil, errors.Wrap(err, "parseTag")
+				}
 
-			// 座標を変えつつ再帰呼び出し
-			texts2, err := parseVarsInternal(v.Field(i), ox+tag.Dx, oy+tag.Dy)
-			if err != nil {
-				return nil, err
-			}
-			texts = append(texts, texts2...)
+				for j := 0; j < v.Field(i).Len(); j++ {
+					fj := float64(j)
+					// 座標を変えつつ再帰呼び出し
+					targets = append(targets, target{v.Field(i).Index(j), tgt.originX + tag.Dx*fj, tgt.originY + tag.Dy*fj})
+				}
 
-		default:
-			return nil, fmt.Errorf("vars の %vフィールドが未対応の型 (%v)です", f.Name, f.Type)
+			case reflect.Struct:
+				tag := &transTag{}
+				if err := tag.parse(tagStr); err != nil {
+					return nil, errors.Wrap(err, "parseTag")
+				}
+
+				// 座標を変えつつ再帰呼び出し
+				targets = append(targets, target{v.Field(i), tgt.originX + tag.Dx, tgt.originY + tag.Dy})
+			default:
+				return nil, fmt.Errorf("vars の %vフィールドが未対応の型 (%v)です", f.Name, f.Type)
+			}
 		}
+
 	}
 
 	return texts, nil
